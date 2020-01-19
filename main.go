9f24a5f242241b63b29f9297b809640587b886bc
Vinayak Mishra <viks@vnykmshr.com>
2020-01-20 00:01:42 +0530

    Adds retries with backoff upon failure

--- main.go
+++ main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"log"
+	"time"
 
+	"github.com/jpillora/backoff"
 	"github.com/vnykmshr/piholebot/pihole"
 )
 
 func main() {
-	err := pihole.NewPiHoleBotModule().DoTheDew()
+	MaxAttempts := 5.0
+
+	b := &backoff.Backoff{
+		Min:    1 * time.Second,
+		Max:    10 * time.Second,
+		Factor: 2,
+		Jitter: true,
+	}
+
+	var err error
+	for {
+		err = pihole.NewPiHoleBotModule().DoTheDew()
+		if err != nil {
+			if b.Attempt() >= MaxAttempts {
+				break
+			}
+
+			d := b.Duration()
+			log.Printf("[pihole][main] error: %s, retrying in %s", err, d)
+			time.Sleep(d)
+			continue
+		}
+
+		b.Reset()
+		break
+	}
+
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("persistent error: %s, retries %f exhausted", err, MaxAttempts)
 	}
 }
